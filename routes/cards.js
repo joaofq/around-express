755bbe6ecb337f166c966d7a51e10bd2b2f254c1
João Fernando <joaofq@hotmail.com>
2022-10-09 12:32:55 -0300

    Refactoring fs with promises

--- routes/cards.js
+++ routes/cards.js
@@ -1,14 +1,17 @@
 const router = require("express").Router();
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const cardsFilePath = path.join(__dirname, "../data/cards.json");
 
 router.get("/cards", (req, res) => {
-  fs.readFile(cardsFilePath, { encoding: "utf8" }, (err, data) => {
-    if (err) throw err;
-    const cards = JSON.parse(data);
-    res.send(cards);
-  });
+  fs.readFile(cardsFilePath, { encoding: "utf8" })
+    .then((data) => {
+      const cardsList = JSON.parse(data);
+      res.send(cardsList);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,62 +1,32 @@
 const router = require("express").Router();
-//const fs = require("fs").promises;
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 const usersFilePach = path.join(__dirname, "../data/users.json");
 
-/*
-Fala Thiago! Blz?
-Estou fazendo o Projeto 12 e fiquei na dúvida sobre qual melhor prática de usar o FS.
-Preciso tratar a requisição do cliente, lendo a lista de usuários que está no servidor (fs.readfile) e respondendo com o usuário definido pelo req.params.id.
-Vi que dá fazer com o fs puro, mas tem muita gente que usa promises no fs.
-To testando a qui esses dois códigos e dá no mesmo resultado. Qual a forma mais recomendada?
-
-*/
-
 router.get("/users/:id", (req, res) => {
-  fs.readFile(usersFilePach, { encoding: "utf8" }, (err, data) => {
-    if (err) throw err;
-    const user = JSON.parse(data).find((obj) => obj._id === req.params.id);
-    if (!user) {
-      res.status(404).send({ message: "ID do usuário não encontrado" });
-    } else {
-      res.send(user);
-    }
-  });
+  fs.readFile(usersFilePach, { encoding: "utf8" })
+    .then((data) => {
+      const user = JSON.parse(data).find((obj) => obj._id === req.params.id);
+      if (!user) {
+        res.status(404).send({ message: "ID do usuário não encontrado" });
+      } else {
+        res.send(user);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-
-// router.get("/users/:id", (req, res) => {
-//   fs.readFile(usersFilePach, { encoding: "utf8" })
-//     .then((data) => {
-//       const user = JSON.parse(data).find((obj) => obj._id === req.params.id);
-//       if (!user) {
-//         res.status(404).send({ message: "ID do usuário não encontrado" });
-//       } else {
-//         res.send(user);
-//       }
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
 
 router.get("/users", (req, res) => {
-  fs.readFile(usersFilePach, { encoding: "utf8" }, (err, data) => {
-    if (err) throw err;
-    const userList = JSON.parse(data);
-    res.send(userList);
-  });
+  fs.readFile(usersFilePach, { encoding: "utf8" })
+    .then((data) => {
+      const usersList = JSON.parse(data);
+      res.send(usersList);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
-
-// router.get("/users", (req, res) => {
-//   fs.readFile(usersFilePach, { encoding: "utf8" })
-//     .then((data) => {
-//       const usersList = JSON.parse(data);
-//       res.send(usersList);
-//     })
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// });
 
 module.exports = router;
